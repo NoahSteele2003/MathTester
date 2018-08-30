307daf40e1cbb5dc3cedd6a6590b44235e6cbfc6
Unknown <noah.steele.2021@student.lhschools.org>
2018-08-30 08:54:41 -0400

    Added more math operations

--- src/MathTester.java
+++ src/MathTester.java
@@ -9,7 +9,6 @@ public class MathTester {
         secondWord ="house";
         firstNumber = 5;
         secondNumber = 10;
-        //hello
 
 
         System.out.println(firstWord+secondWord);
@@ -19,7 +18,7 @@ public class MathTester {
         System.out.println("secondNumber / firstNumber is "+ (secondNumber/firstNumber));
         System.out.println("firstNumber * secondNumber is "+ (firstNumber*secondNumber));
         System.out.println("The modulus of the secondNumber by the firstNumber is "+ (secondNumber%firstNumber));
-        
+
 
     }
 }
