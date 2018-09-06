d6c737fda4dd4f5d08faa4a597d484f83398b5c8
Unknown <noah.steele.2021@student.lhschools.org>
2018-09-06 08:49:28 -0400

    Finished project

--- src/MathTester.java
+++ src/MathTester.java
@@ -1,14 +1,17 @@
+import java.util.Scanner;
+
 public class MathTester {
     public static void main(String[] args) {
 
         String firstWord;
         String secondWord;
-        int firstNumber,secondNumber;
+        int firstNumber,secondNumber,total;
 
         firstWord="dog";
         secondWord ="house";
         firstNumber = 5;
         secondNumber = 10;
+        total = firstNumber+secondNumber;
 
 
         System.out.println(firstWord+secondWord);
@@ -18,6 +21,17 @@ public class MathTester {
         System.out.println("secondNumber / firstNumber is "+ (secondNumber/firstNumber));
         System.out.println("firstNumber * secondNumber is "+ (firstNumber*secondNumber));
         System.out.println("The modulus of the secondNumber by the firstNumber is "+ (secondNumber%firstNumber));
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
